48582cf0454bea79b3a68b48f3b04d72aa33c273
s225730622 <s225730622@deakin.edu.au>
2026-04-20 04:20:45 +1000

    Update Task4.1Interrupts.ino

--- Task4.1Interrupts/Task4.1Interrupts.ino
+++ Task4.1Interrupts/Task4.1Interrupts.ino
@@ -1,10 +1,17 @@
 // Task 4.1P - Handling Interrupts
+#include <Wire.h>
+#include <BH1750.h> 
 
-// Define pins for LEDs, switch, and PIR sensor
+// Initialize light meter
+BH1750 lightMeter;
+// Initialize dark environment threshold, leave a tiny bit of light so Linda is not in complete darkness
+const float darkness = 0.5;
+
+// Define pins for LEDs, switch and PIR sensor
 const int switchPin = 3;
 const int pirPin = 2;
-const int led1 = 7;
-const int led2 = 8;
+const int led1 = 7;   // White LED
+const int led2 = 8;   // Yellow LED
 
 // Shared variables updated by interrupts
 volatile uint8_t switchState = HIGH;    // HIGH = switch OFF, LOW = switch ON
@@ -23,7 +30,13 @@ void setup() {
   pinMode(pirPin, INPUT);               
   pinMode(switchPin, INPUT_PULLUP);     // Inverts ON/OFF values
 
-  Serial.begin(9600);
+  Wire.begin();
+
+  if (lightMeter.begin()) {
+  Serial.println("BH1750 started");
+  } else {
+  Serial.println("BH1750 failed to start");
+  }
 
   // Attach interrupts
   attachInterrupt(digitalPinToInterrupt(switchPin), switchInterrupt, CHANGE);
@@ -31,33 +44,42 @@ void setup() {
 }
 
 void loop() {
-  // Print lights on/lights off message depending on interrupt and state
+  float lightLvl = readLight(); 
+  bool isDark = (lightLvl <= darkness);
+
+  // Interrupt events to print respective messages
+  if (sensorEvent) {
+    // Reset the boolean trigger 
+    sensorEvent = false;
+
+    // Print appropriate message
+    if (sensorState == HIGH && isDark) {
+      Serial.println("Motion detected, lights ON");
+    }
+    else if ((sensorState == LOW && isDark) || !isDark) {
+      Serial.println("No motion detected, lights OFF");
+    }
+  }
+
   if (switchEvent) {
     // Reset the boolean trigger 
     switchEvent = false;
 
     if (switchState == LOW) {
       Serial.println("Lights switched ON");
-      lightsOn();     // Turn LED lights ON
     }
     else {
       Serial.println("Lights switched OFF");
-      lightsOff();    // Turn LED lights OFF
     }
   }
-  if (sensorEvent) {
-    // Reset the boolean trigger 
-    sensorEvent = false;
 
-    if (sensorState == HIGH) {
-      Serial.println("Motion detected, lights ON");
-      lightsOn();     // Turn LED lights ON
-    }
-    else {
-      Serial.println("No motion detected, lights OFF");
-      lightsOff();    // Turn LED lights OFF
-    }
+  // Turn lights on automatically if it is dark
+  if ((isDark && sensorState == HIGH) || switchState == LOW) {
+    lightsOn();     // Turn LED lights ON
   }
+  else {
+    lightsOff();    // Turn LED lights OFF
+  } 
   delay(100);
 }
 
@@ -83,4 +105,9 @@ void lightsOn() {
 void lightsOff() {
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
+}
+
+float readLight() {
+  float lux = lightMeter.readLightLevel();
+  return lux;
 }
